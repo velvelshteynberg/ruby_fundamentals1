16e1c347b414ad53a933bc9f5199b7c4712d4c26
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-06 20:31:20 -0500

    part one of the excercise on Ruby

---
+++ excercise1.rb
@@ -0,0 +1,32 @@
+#practicing adding numbers
+
+# puts 2
+# puts 3
+# puts 2+3
+
+# puts 5+3
+# puts 5-3
+# puts 5*3
+# puts 5/3
+# puts 5/3.0
+# puts 5 % 3
+
+# puts 5 > 3
+# puts 5 < 3 
+# puts 5 > 5
+# puts 5 >= 5
+
+# 2 == 2 
+# 2 == 3
+
+# puts 'Hello world'
+# puts "Hello world"
+# puts "Betty's pie shop"
+# puts 'Betty\'s pie shop'
+
+# puts "Ada lovelace lived for #{1852-1815}"
+
+ puts (1 < 3) && ( 3 < 5 )
+ puts (1 < 3) || ( 3 < 5 )
+ puts (1 > 3) && ( 3 < 5 )
+ puts (1 == 3) && ( 3 < 5 )
