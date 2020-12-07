8b716462546b8336bc98117461a73c0c14f32955
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-06 21:05:16 -0500

    practicing boolean

---
+++ excercise2.rb
@@ -0,0 +1,31 @@
+#good tip for a $55 meal
+puts "This #{55.0 * 0.15} dollar tip is for you"
+
+#converting integer to string and adding
+fav_num = 35
+fav_activity = "showering"
+puts fav_num.to_s + fav_activity
+
+#string interpolation
+puts "45628 multiplied by 7839 is #{45628*7839}"
+
+#no idea what this means
+# What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+
+#practicing variables
+name = "Velvel"
+greeting = "Hello #{name}! today is an amazing day"
+mission = "My mission, and I choose to accept it...."
+puts "#{greeting} #{mission}"
+
+#question- when I add two strings there is not space in between them. how do i change that
+
+# my_number = 12
+# my_number > 11
+# your_number = 1
+
+# my_number >= your_number
+
+amount = 30 
+amount -= 5
+puts "#{amount}"
